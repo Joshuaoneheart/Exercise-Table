1b87b0c6bacb1e5663639ae6e9ccbf235b861f52
Joshuaoneheart <joshua@DESKTOP-LLRE8OQ.localdomain>
2021-07-17 03:50:20 +0800

    Finish app.js using express and ejs and update index.html to the newest version.

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express();
+
+app.engine('html', require('ejs').renderFile);
+app.set("view engine", "ejs");
+app.set('views', __dirname);
+
+app.get("/", function(req, res){
+  res.render("index.html", {"title": "台大台科大操練表系統"});
+})
+
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running...")
+)
