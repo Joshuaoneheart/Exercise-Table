387a27e9a31bb328f4169b46316862de93dee8ea
Joshuaoneheart <luna920804@DESKTOP-4083ONV.localdomain>
2021-07-30 15:14:23 +0800

    new template and its login system

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import './scss/style.scss';
+import {
+	  FirebaseAuthProvider,
+	  FirebaseAuthConsumer
+} from "@react-firebase/auth";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const loading = (
+  <div className="pt-3 text-center">
+    <div className="sk-spinner sk-spinner-pulse"></div>
+  </div>
+)
+var config = {
+	    apiKey: "AIzaSyBRYT6ipwBqNlt8xqkU2NfPV5XpU0PXxsE",
+	    authDomain: "exercising-table-data.firebaseapp.com",
+	    databaseURL: "https://exercising-table-data-default-rtdb.firebaseio.com",
+	    projectId: "exercising-table-data",
+	    storageBucket: "exercising-table-data.appspot.com",
+	    messagingSenderId: "820908125256",
+	    appId: "1:820908125256:web:dbd81b7b5fcadf7c743c7d",
+	    measurementId: "G-TJ2TJXC3N7"
+};
+
+// Containers
+const TheLayout = React.lazy(() => import('./containers/TheLayout'));
+
+// Pages
+const Login = React.lazy(() => import('./views/pages/login/Login'));
+const Register = React.lazy(() => import('./views/pages/register/Register'));
+const Page404 = React.lazy(() => import('./views/pages/page404/Page404'));
+const Page500 = React.lazy(() => import('./views/pages/page500/Page500'));
+
+class App extends Component {
+
+  render() {
+    return (
+      <HashRouter>
+          <React.Suspense fallback={loading}>
+			<FirebaseAuthProvider {...config} firebase={firebase}>
+				<Switch>
+				  <Route exact path="/register" name="Register Page" render={props => <Register {...props}/>} />
+				  <Route exact path="/404" name="Page 404" render={props => <Page404 {...props}/>} />
+				  <Route exact path="/500" name="Page 500" render={props => <Page500 {...props}/>} />
+				  <Route path="/" name="Home" render={props => 
+					  <FirebaseAuthConsumer>
+					  	{({ isSignedIn, user, providerId }) => {
+							console.log(isSignedIn, user, providerId)
+							if(isSignedIn && user.email === "admin@hall19.com") return <TheLayout firebase={firebase}/>;
+							else return <Login firebase={firebase} />;
+						}}
+					  </FirebaseAuthConsumer>
+				  	} 
+				  />	
+				</Switch>
+			</FirebaseAuthProvider>
+          </React.Suspense>
+      </HashRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/views/pages/login/Login.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCardGroup,
+  CCol,
+  CContainer,
+  CForm,
+  CInput,
+  CInputGroup,
+  CInputGroupPrepend,
+  CInputGroupText,
+  CRow
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+
+const Login = (props) => {
+  var username = React.createRef();
+  var password = React.createRef();
+  console.log(username)
+  return (
+    <div className="c-app c-default-layout flex-row align-items-center">
+      <CContainer>
+        <CRow className="justify-content-center">
+          <CCol md="8">
+            <CCardGroup>
+              <CCard className="p-4">
+                <CCardBody>
+                  <CForm>
+                    <h1>Login</h1>
+                    <p className="text-muted">Sign In to your account</p>
+                    <CInputGroup className="mb-3">
+                      <CInputGroupPrepend>
+                        <CInputGroupText>
+                          <CIcon name="cil-user" />
+                        </CInputGroupText>
+                      </CInputGroupPrepend>
+                      <CInput innerRef={username} type="text" placeholder="Username" autoComplete="username" />
+                    </CInputGroup>
+                    <CInputGroup className="mb-4">
+                      <CInputGroupPrepend>
+                        <CInputGroupText>
+                          <CIcon name="cil-lock-locked" />
+                        </CInputGroupText>
+                      </CInputGroupPrepend>
+                      <CInput innerRef={password}  type="password" placeholder="Password" autoComplete="current-password" />
+                    </CInputGroup>
+                    <CRow>
+                      <CCol xs="6">
+                        <CButton color="primary" className="px-4" onClick={() => props.firebase.auth().signInWithEmailAndPassword(username.current.value, password.current.value).catch((error) => window.alert("Error : " + error.message))}>Login</CButton>
+                      </CCol>
+                      <CCol xs="6" className="text-right">
+                        <CButton color="link" className="px-0">Forgot password?</CButton>
+                      </CCol>
+                    </CRow>
+                  </CForm>
+                </CCardBody>
+              </CCard>
+            </CCardGroup>
+          </CCol>
+        </CRow>
+      </CContainer>
+    </div>
+  )
+}
+
+export default Login
