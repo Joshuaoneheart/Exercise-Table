d49cad9bee111f85a5d594f760f93773b575f3f9
Joshuaoneheart <joshuayou@LAPTOP-6A6MV2G5.localdomain>
2021-08-20 15:49:57 +0800

    fix appearance of Form.js

--- src/views/admin/form/Form.js
+++ src/views/admin/form/Form.js
@@ -29,6 +29,9 @@ import { loading } from 'src/reusable'
 import { FirestoreCollection } from '@react-firebase/firestore'
 const Problem = (props) => {
 	var frame = [];
+	var option_style={color: "#000000", fontSize: "20px"};
+	var title_style={color: "#636f83"};
+	var button_style={height: "20px", width: "20px"};
 	console.log(props.data);
 	switch(props.data.type){
 		case "Grid":
@@ -36,15 +39,15 @@ const Problem = (props) => {
 			var row = [];
 			option_row.push(<CCol></CCol>);
 			for(var option of props.data["選項"].split(";")){
-				option_row.push(<CCol>{option}</CCol>)
+				option_row.push(<CCol style={Object.assign({}, option_style, {textAlign: "center"})}>{option}</CCol>)
 			}
 			for(var suboption of props.data["子選項"].split(";")){
 				var subframe = [];
-				subframe.push(<CCol>{suboption}</CCol>);
+				subframe.push(<CCol style={option_style}>{suboption}</CCol>);
 				for(var option of props.data["選項"].split(";")){
 					subframe.push(
-						<CCol>
-					   		<CInputRadio className="form-check-input" name={suboption} value={option} style={{width:"15px",height:"15px"}}/>
+						<CCol style={{display: "flex", alignItems: "center",justifyContent: "center"}}>
+					   		<CInputRadio className="form-check-input" name={suboption} value={option} style={Object.assign({}, button_style, {marginLeft: "2px"})}/>
 						</CCol>
 					)
 				}
@@ -67,8 +70,8 @@ const Problem = (props) => {
 			for(var option of props.data["選項"].split(";")){
 				frame.push(
 				<CFormGroup variant="checkbox">
-				   <CInputRadio className="form-check-input" name={props.data.title} value={option} style={{width:"15px",height:"15px"}}/>
-				   <CLabel variant="checkbox">{option}</CLabel>
+				   <CInputRadio className="form-check-input" name={props.data.title} value={option} style={button_style}/>
+				   <CLabel variant="checkbox" style={Object.assign({}, option_style, {marginLeft:"10px"})}>{option}</CLabel>
 				</CFormGroup>);
 			}
 			break;
@@ -76,16 +79,16 @@ const Problem = (props) => {
 			for(var option of props.data["子選項"].split(";")){
 				frame.push(
 				<CFormGroup variant="checkbox">
-				   <CInputCheckbox className="form-check-input" name={props.data.title} value={option} style={{width:"15px",height:"15px"}} />
-				   <CLabel variant="checkbox">{option}</CLabel>
+				   <CInputCheckbox className="form-check-input" name={props.data.title} value={option} style={button_style} />
+				   <CLabel variant="checkbox" style={Object.assign({}, option_style, {marginLeft:"10px"})}>{option}</CLabel>
 				</CFormGroup>);
 			}
 			break;
 	}
 	return (
 		<>
-		<CFormGroup>
-			<h4>{props.data.title}</h4>
+		<CFormGroup style={{marginBottom:"25px"}}>
+			<h4 style={title_style}>{props.data.title}</h4>
 			<hr />
 			<CCol>
 				{frame}
@@ -138,8 +141,8 @@ const DataTabs = (props) => {
 				</CTabs>
 			</CForm>
 		  </CCardBody>
-		  <CCardFooter>
-		  </CCardFooter>
+		{props.submitable && <CCardFooter>
+		  </CCardFooter>}
 		</CCard>
 	);
 }
