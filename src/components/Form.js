97c52177f00c43fdc0dfc1d0ca58e9fbec4e5f58
Joshuaoneheart <b08902017@csie.ntu.edu.tw>
2023-02-12 14:16:52 +0800

    add GatherProblemsBySection

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,10 +3,24 @@ import { FirestoreCollection } from "@react-firebase/firestore";
 import DataTabs from "./DataTab";
 import loading from "./loading";
 
+const GatherProblemsBySection = (d) => {
+  var data = { value: [], sections: [] };
+  for (var i = 0; i < d.value.length; i++) {
+    // assign unique id to problem
+    d.value[i].id = d.ids[i];
+    if (!data.sections.includes(d.value[i].section)) {
+      data.sections.push(d.value[i].section);
+      data.value.push([]);
+    }
+    data.value[data.sections.indexOf(d.value[i].section)].push(d.value[i]);
+  }
+  return data;
+};
+
 const Form = ({ is_footer, default_data, account }) => {
   return (
-      <FirestoreCollection path="/form/">
-        {/* 
+    <FirestoreCollection path="/form/">
+      {/* 
             receiving form data from Firebase
             format of d.value
             {
@@ -17,35 +31,24 @@ const Form = ({ is_footer, default_data, account }) => {
                 選項: separated by ';'
             }
          */}
-        {(d) => {
-          if (d.isLoading) return loading;
-          if (d && d.value) {
-            var data = { value: [], sections: [] };
-            for (var i = 0; i < d.value.length; i++) {
-              // assign unique id to problem
-              d.value[i].id = d.ids[i];
-              if (!data.sections.includes(d.value[i].section)) {
-                data.sections.push(d.value[i].section);
-                data.value.push([]);
-              }
-              data.value[data.sections.indexOf(d.value[i].section)].push(
-                d.value[i]
-              );
-            }
-            return (
-              <CCol>
-                <DataTabs
-                  data={data}
-                  metadata={d}
-                  is_footer={is_footer}
-                  default_data={default_data}
-                  account={account}
-                />
-              </CCol>
-            );
-          } else return null;
-        }}
-      </FirestoreCollection>
+      {(d) => {
+        if (d.isLoading) return loading;
+        if (d && d.value) {
+          var data = GatherProblemsBySection(d);
+          return (
+            <CCol>
+              <DataTabs
+                data={data}
+                metadata={d}
+                is_footer={is_footer}
+                default_data={default_data}
+                account={account}
+              />
+            </CCol>
+          );
+        } else return null;
+      }}
+    </FirestoreCollection>
   );
 };
 
