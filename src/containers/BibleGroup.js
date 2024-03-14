0da03422ab103d7b0480cdad65d4f615d7b8397e
Joshuaoneheart <r12922220@ntu.edu.tw>
2024-03-14 16:52:16 +0800

    update Chart for Number and MultiGrid

--- src/containers/BibleGroup.js
+++ src/containers/BibleGroup.js
@@ -4,7 +4,7 @@ import { FirestoreCollection } from "@react-firebase/firestore";
 import Groups from "Models/Groups";
 import { loading } from "components";
 import { DB } from "db/firebase";
-import { GroupContext } from "hooks/context";
+import { AccountsMapContext, GroupContext } from "hooks/context";
 import { useCallback, useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import { GetWeeklyBase } from "utils/date";
@@ -22,7 +22,7 @@ const colors = [
   "rgba(226,174,221,1)",
   "rgba(255,232,194,1)",
   "rgba(228,255,225,1)",
-  "rgba(161,134,158,1)"
+  "rgba(161,134,158,1)",
 ];
 const a_colors = [
   "rgba(255,99,132,0.4)",
@@ -34,7 +34,7 @@ const a_colors = [
   "rgba(226,174,221,0.4)",
   "rgba(255,232,194,0.4)",
   "rgba(228,255,225,0.4)",
-  "rgba(161,134,158,0.4)"
+  "rgba(161,134,158,0.4)",
 ];
 const aa_colors = [
   "rgba(255,99,132,0.2)",
@@ -46,7 +46,7 @@ const aa_colors = [
   "rgba(226,174,221,0.2)",
   "rgba(255,232,194,0.2)",
   "rgba(228,255,225,0.2)",
-  "rgba(161,134,158,0.2)"
+  "rgba(161,134,158,0.2)",
 ];
 
 // FIXME:
@@ -112,8 +112,9 @@ const RenderPieChart = ({ title, labels, data }) => {
 const ProblemChart = ({ problem, data }) => {
   let options;
   let suboptions;
+  const accountsMap = useContext(AccountsMapContext);
   if (problem.type === "MultiChoice") {
-    options = problem["選項"].split(";");
+    options = problem["選項"];
     let polar_area = [];
     for (let i = 0; i < options.length; i++) polar_area.push(0);
     for (let i = 0; i < data.length; i++) {
@@ -131,7 +132,7 @@ const ProblemChart = ({ problem, data }) => {
       />
     );
   } else if (problem.type === "MultiAnswer") {
-    suboptions = problem["子選項"].split(";");
+    suboptions = problem["子選項"];
     let h_bar_data = [];
     for (let i = 0; i < suboptions.length; i++) h_bar_data.push(0);
     for (let i = 0; i < data.length; i++) {
@@ -150,8 +151,8 @@ const ProblemChart = ({ problem, data }) => {
       />
     );
   } else if (problem.type === "Grid") {
-    suboptions = problem["子選項"].split(";");
-    options = problem["選項"].split(";");
+    suboptions = problem["子選項"];
+    options = problem["選項"];
     let bar_data = [];
     for (let i = 0; i < suboptions.length; i++) {
       bar_data.push([]);
@@ -160,7 +161,7 @@ const ProblemChart = ({ problem, data }) => {
         if (
           data[j][problem.id] &&
           data[j][problem.id][suboptions[i]] &&
-          options.indexOf(data[j][problem.id][suboptions[i]].ans) !== -1
+          options.includes(data[j][problem.id][suboptions[i]].ans)
         )
           bar_data[i][
             options.indexOf(data[j][problem.id][suboptions[i]].ans)
@@ -172,6 +173,43 @@ const ProblemChart = ({ problem, data }) => {
         title={problem.title}
         titles={suboptions}
         labels={options}
+        data={bar_data}
+      />
+    );
+  } else if (problem.type === "MultiGrid") {
+    suboptions = problem["子選項"];
+    options = problem["選項"];
+    let bar_data = [];
+    for (let i = 0; i < options.length; i++) {
+      bar_data.push([]);
+      for (let j = 0; j < suboptions.length; j++) bar_data[i].push(0);
+      for (let j = 0; j < data.length; j++) {
+        if (data[j][problem.id] && data[j][problem.id][options[i]])
+          for (let k = 0; k < data[j][problem.id][options[i]].ans.length; k++) {
+            if (suboptions.includes(data[j][problem.id][options[i]].ans[k]))
+              bar_data[i][
+                suboptions.indexOf(data[j][problem.id][options[i]].ans[k])
+              ]++;
+          }
+      }
+    }
+    return (
+      <RenderBarChart
+        title={problem.title}
+        titles={options}
+        labels={suboptions}
+        data={bar_data}
+      />
+    );
+  } else if (problem.type == "Number") {
+    let bar_data = [
+      data.map((x) => (x[problem.id] ? parseInt(x[problem.id].ans) : 0)),
+    ];
+    return (
+      <RenderBarChart
+        title={problem.title}
+        titles={[problem.title]}
+        labels={data.map((x) => accountsMap[x.id])}
         data={bar_data}
       />
     );
@@ -187,14 +225,14 @@ const ProblemStatistic = ({ problems, groups, group_id }) => {
     let tmpData = [];
     let now = GetWeeklyBase();
     for (let i = 0; i < accounts.length; i++) {
-      let user_data = {};
+      let user_data = { id: accounts.list[i].id };
       try {
         let collection = await DB.getByUrl(
           "/accounts/" + accounts.list[i].id + "/data"
         );
         collection.forEach((doc) => {
           if (doc.exists && parseInt(doc.id) === now) {
-            user_data = doc.data();
+            user_data = Object.assign(user_data, doc.data());
           }
         });
       } catch (e) {
