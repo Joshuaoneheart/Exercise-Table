c40fd49a90d0233cbc57c261cfd8339c3265a550
Joshuaoneheart <b08902017@csie.ntu.edu.tw>
2024-06-14 20:30:56 +0800

    fix a bug

--- src/utils/problem.js
+++ src/utils/problem.js
@@ -29,6 +29,11 @@ const SummaryScore = async (data, problems, id) => {
     cur_lord_table: 0,
   };
   const lord_table_id = "0it0L8KlnfUVO1i4VUqi";
+  // record if the problem is used in this semester
+  let problem_used = {};
+  for (let problem of problems) {
+    problem_used[problem.id] = 0;
+  }
   for (let i = 0; i < data.value.length; i++) {
     items.push({
       week_base: WeeklyBase2String(parseInt(data.ids[i])),
@@ -48,14 +53,18 @@ const SummaryScore = async (data, problems, id) => {
         items[i][problem.id] = 0;
       if (problem.type === "GF") items[i][problem.id] = 0;
       if (problem.type === "GF" && problem.title in data.value[i]) {
+        problem_used[problem.id] += 1;
         items[i][problem.id] = data.value[i][problem.title].length;
       }
       if (!(problem.id in data.value[i])) continue;
       if (problem.type === "MultiChoice") {
         items[i][problem.id] = data.value[i][problem.id].ans;
+        problem_used[problem.id] += 1;
       } else if (problem.type === "MultiAnswer") {
         items[i][problem.id] = data.value[i][problem.id].ans.length;
+        problem_used[problem.id] += 1;
       } else if (problem.type === "Grid") {
+        problem_used[problem.id] += 1;
         for (let suboption of problem["子選項"]) {
           if (suboption in data.value[i][problem.id]) {
             items[i][problem.id + "-" + suboption] =
@@ -63,6 +72,7 @@ const SummaryScore = async (data, problems, id) => {
           }
         }
       } else if (problem.type === "MultiGrid") {
+        problem_used[problem.id] += 1;
         for (let option of problem["選項"]) {
           if (option in data.value[i][problem.id]) {
             items[i][problem.id + "-" + option] =
@@ -70,10 +80,10 @@ const SummaryScore = async (data, problems, id) => {
           }
         }
       } else if (problem.type === "Number") {
+        problem_used[problem.id] += 1;
         items[i][problem.id] = data.value[i][problem.id].ans;
       }
     }
-
     items[i].score = data.value[i].scores;
     items[i]["召會生活操練"] = data.value[i]["召會生活操練"];
     items[i]["神人生活操練"] = data.value[i]["神人生活操練"];
@@ -100,6 +110,17 @@ const SummaryScore = async (data, problems, id) => {
       if (data.value[i][lord_table_id])
         result.cur_lord_table =
           data.value[i][lord_table_id].ans === "有" ? 1 : 0;
+    }
+  }
+
+  for (let problem of problems) {
+    for (let i = 0; i < items.length; i++) {
+      if (problem_used[problem.id] === 0) {
+        console.log(problem);
+        for (let [k, v] of Object.entries(items[i])) {
+          if (k.includes(problem.id)) delete items[i][k];
+        }
+      }
     }
   }
   return { items, result };
